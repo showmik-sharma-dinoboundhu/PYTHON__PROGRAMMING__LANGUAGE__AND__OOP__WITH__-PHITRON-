65ea74da96fee757aa0a7bd75860ef6b994d2f94
Showmik Sharma <showmiksharma2180@gmail.com>
2026-08-04 09:08:10 +0600

    Add files via upload

---
+++ Practice_Python/Day-06/A_Restuarant.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+
+class User(ABC):
+    def __init__(self, name, phone, email, add) -> None:
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.address = add
+
+class Customer(User):
+    def __init__(self, name, money, phone, email, add) -> None:
+        self.wallet = money
+        self.__order = None
+        super().__init__(name, phone, email, add)
+
+    @property
+    def order(self):
+        return self.order
+
+    @order.setter
+    def order(self, order):
+        self.__order = order
+
+    def place_of_Order(self, order):
+        self.order = order
+        return(f"{self.name} in the order")
+
+    def place_order(self, order):
+        self.order = order
+        return(f"{self.name} for the man")
+
+    def eat_food(self, order):
+        return(f"{self.name} with items of{order.items}")
+
+    def pay_for_order(self, order):
+        pass
+
+    def give_tips(self, tips_amount):
+        pass
+
+    def give_review(self, stars):
+        pass
+
+
+class Employee(User):
+    def __init__(self, name, salary, starting_date, department, phone, email, add) -> None:
+           super().__init__(name, phone, email, add)
+           self.salary = salary
+           self.starting_date = starting_date
+           self.department = department
+           self.starting_date = starting_date
+           self.department = department
+
+
+class Chef(Employee):
+    def 
+
+
+        
